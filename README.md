e50d813f411574f4b57523f372eb4b7b3ade30a1
Majid Naave <naavemajid2001@gmail.com>
2023-11-01 10:52:29 -0800

    final work

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frontend Mentor - Newsletter sign-up form with success message
+
+# Description
+This a project l took from Frontend mentor. A newslettter sign-up form using HTML, CSS, and JAVASCRIPT.
+
+
+# demo 
+
+![Alt text](2023-11-01.png)
+
+# Success Message
+
+![Alt text](<2023-11-01 (1).png>)
--- script.js
+++ script.js
@@ -2,15 +2,29 @@ const btn = document.getElementById('btn');
 const success = document.getElementById('success');
 const dismiss = document.getElementById('dismiss');
 const container = document.querySelector('.container');
-
-
+const emailValue = document.getElementById('email');
+const errorValue = document.getElementById('error');
 
 
 
 btn.addEventListener('click', (e)=>{
     e.preventDefault()
-    success.style.display= 'block';
-    container.style.display = 'none';
+    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+     const isValidEmail = emailRegex.test(emailValue.value);
+
+
+    if( !(isValidEmail) || emailValue.value === ''){
+        errorValue.innerText = 'valid email required';
+        errorValue.classList.add('error');
+        emailValue.classList.add('email');
+
+        
+    }else{
+     emailValue.style.display ='block'
+     success.style.display= 'block';
+     container.style.display = 'none';
+
+    }
 
 })
 
